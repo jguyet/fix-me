319cfb3938fd6f2d974fa7789df5e296e3dd2996
Jeremy GUYET <jguyet@e1r11p4.42.fr>
2017-12-01 05:32:47 +0100

    fixme-router ASynchronousServerSocketHandler started (connection OK, disconnection OK, message reader "BufferReader" OK)

--- fixme-router/src/main/java/org/fixme/router/App.java
+++ fixme-router/src/main/java/org/fixme/router/App.java
@@ -1,17 +1,57 @@
 package org.fixme.router;
 
 /**
- * Hello world!
- *
+ * Main application.
+ * @author jguyet
  */
 public class App 
-{
+{	
+	public static BrokerServerHandler brokerHandler;
+	public static MarketServerHandler marketHandler;
+	
+	private static boolean stopped = false;
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	catchSigTerm();
+    	
+    	//################################################################################################
+    	//BUILD HANDLERS
+    	buildHandlers();
+    	//################################################################################################
+    	//START HANDLERS
+    	runHandlers();
+    	//################################################################################################
+    	//WAIT STOPPED SIG
+    	while (stopped == false) {
+    		try { Thread.sleep(500); } catch(Exception e) { e.printStackTrace(); }
+    	}
+    	//################################################################################################
+    	//STOP HANDLERS
+    	stopHandlers();
     }
     
-    public static void testttt() {
-    	System.out.println( "DEPENDECIE OF ROUTER" );
+    private static void buildHandlers() {
+    	brokerHandler = new BrokerServerHandler(RouterProperties.BROKER_ASYNCHRONOUS_SERVER_CHANNEL_PORT);
+    	marketHandler = new MarketServerHandler(RouterProperties.MARKET_ASYNCHRONOUS_SERVER_CHANNEL_PORT);
+    }
+    
+    private static void runHandlers() {
+    	brokerHandler.run();
+    	marketHandler.run();
+    }
+    
+    private static void stopHandlers() {
+    	brokerHandler.stop();
+    	marketHandler.stop();
+    }
+    
+    private static void catchSigTerm() {
+    	Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+                public void run() {
+                    System.out.println("Inside Add Shutdown Hook");
+                }   
+            }); 
     }
 }
---
+++ fixme-router/src/main/java/org/fixme/router/RouterProperties.java
@@ -0,0 +1,12 @@
+package org.fixme.router;
+
+/**
+ * Properties
+ * @author jguyet
+ */
+public class RouterProperties {
+	
+	public static final int BROKER_ASYNCHRONOUS_SERVER_CHANNEL_PORT = 5000;
+	
+	public static final int MARKET_ASYNCHRONOUS_SERVER_CHANNEL_PORT = 5001;
+}
